e213cb9127137a6b47b8bf0b4ece912e4643828c
Paul Abrams <john.paul.abrams@gmail.com>
2025-03-28 18:55:09 -0400

    chore: move deps

--- cm-chessboard/ChessboardState.js
+++ cm-chessboard/ChessboardState.js
@@ -3,7 +3,7 @@
  * Repository: https://github.com/shaack/cm-chessboard
  * License: MIT, see file 'LICENSE'
  */
-import {Position} from "cm-chessboard/src/model/Position.js"
+import {Position} from "./model/Position.js"
 
 export class ChessboardState {
 
